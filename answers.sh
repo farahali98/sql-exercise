9a8bc9b7673a303e43e11410c20a567717cd9d44
Farah-Ali <farah.ali9805@gmail.com>
2020-10-16 12:41:53 +0300

    joins

--- answers.sh
+++ answers.sh
@@ -29,3 +29,38 @@ SET
     Points = 194
 WHERE
     name = 'Alex';
+
+
+CREATE TABLE Graduates(
+ ID INTEGER PRIMARY KEY AUTOINCREMENT,
+   Name TEXT NOT NULL UNIQUE,
+   Age INTEGER,
+   Gender TEXT,
+   Points TEXT NOT NULL,
+   Graduation varchar(255));
+
+
+
+
+INSERT INTO Graduates(ID, Name, Age, Gender,Points)
+SELECT ID , Name, Age,Gender,Points FROM students
+WHERE Name='Layal';
+
+
+
+UPDATE Graduates
+SET Graduation='08/09/2018'
+WHERE name='Layal';
+
+
+
+SELECT  employees.name,companies.name, companies.Date
+FROM companies
+INNER JOIN employees
+ON companies.name=employees.Company;
+
+
+
+select employees.Name from employees join companies 
+companies on companies.Name= employees.Company where 
+companies.Date < 2000
